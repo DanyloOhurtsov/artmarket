6af8c94e49e12d6c91d0df177a15164fb690b4d3
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2025-01-02 20:40:01 +0100

    bug fix

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -1,64 +1,64 @@
-import type { CollectionConfig, PayloadRequest } from "payload";
+import type { CollectionConfig } from "payload"
 
 export const Products: CollectionConfig = {
-  slug: "products",
-  access: {
-    read: () => true,
-  },
-  admin: {
-    defaultColumns: ["title", "slug", "images", "category"],
-    useAsTitle: "title",
-  },
-  fields: [
-    {
-      name: "title",
-      label: "Назва товару",
-      type: "text",
-      required: true,
+    slug: "products",
+    access: {
+        read: () => true,
     },
-    {
-      name: "slug",
-      label: "Назва товару в URL",
-      type: "text",
-      required: true,
+    admin: {
+        defaultColumns: ["title", "slug", "images", "category"],
+        useAsTitle: "title",
     },
-    {
-      name: "description",
-      label: "Опис товару",
-      type: "richText",
-    },
-    {
-      name: "price",
-      label: "Ціна товару",
-      type: "number",
-      defaultValue: 0,
-      required: true,
-    },
-    {
-      name: "category",
-      label: "Категорія товару",
-      type: "relationship",
-      relationTo: "categories",
-      required: true,
-      admin: {
-        position: "sidebar",
-      },
-    },
-
-    {
-      name: "images",
-      label: "Зображення товару",
-      type: "array",
-      minRows: 1,
-      required: true,
-      fields: [
+    fields: [
         {
-          name: "image",
-          label: "Зображення",
-          relationTo: "media",
-          type: "upload",
+            name: "title",
+            label: "Назва товару",
+            type: "text",
+            required: true,
         },
-      ],
-    },
-  ],
-};
+        {
+            name: "slug",
+            label: "Назва товару в URL",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "description",
+            label: "Опис товару",
+            type: "richText",
+        },
+        {
+            name: "price",
+            label: "Ціна товару",
+            type: "number",
+            defaultValue: 0,
+            required: true,
+        },
+        {
+            name: "category",
+            label: "Категорія товару",
+            type: "relationship",
+            relationTo: "categories",
+            required: true,
+            admin: {
+                position: "sidebar",
+            },
+        },
+
+        {
+            name: "images",
+            label: "Зображення товару",
+            type: "array",
+            minRows: 1,
+            required: true,
+            fields: [
+                {
+                    name: "image",
+                    label: "Зображення",
+                    relationTo: "media",
+                    type: "upload",
+                },
+            ],
+        },
+    ],
+}
