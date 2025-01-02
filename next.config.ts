a7379c6986d70d2f00ee3f39bfa7d6506cd6cfff
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2025-01-02 20:43:04 +0100

    Temporarily remove i18n

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["en-US", "uk-UA"],
-    defaultLocale: "uk-UA",
-  },
+  // i18n: {
+  //   locales: ["en-US", "uk-UA"],
+  //   defaultLocale: "uk-UA",
+  // },
 };
 
 export default withPayload(nextConfig);
